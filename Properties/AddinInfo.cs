dfdee6ed11907a9c0a921429d94feca3959ad423
David Karlaš <david.karlas@xamarin.com>
2014-10-26 18:44:03 +0100

    Initial version

---
+++ Properties/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"VSBindingsOnMac", 
+	Namespace = "VSBindingsOnMac",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("VSBindingsOnMac")]
+[assembly:AddinCategory ("VSBindingsOnMac")]
+[assembly:AddinDescription ("VSBindingsOnMac")]
+[assembly:AddinAuthor ("David Karlaš")]
+
+[assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
